6810cf67978ef4a2fb5b135f1f7ed5713c2ced4e
Henrique Levites <78315738+henriquelevites@users.noreply.github.com>
2021-02-04 16:43:27 +0200

    Add files via upload
    Added explanations to lines in comments

---
+++ src/main/java/pageObjects/mobileApiDemos/ThirdMenuPage.java
@@ -0,0 +1,34 @@
+package pageObjects.mobileApiDemos;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class ThirdMenuPage {
+
+    private AppiumDriver mobileDriver;
+
+    public ThirdMenuPage(AppiumDriver mobileDriver) {
+        this.mobileDriver = mobileDriver;
+//        PageFactory.initElements(new AppiumFieldDecorator(mobileDriver, 3, TimeUnit.SECONDS), this);  (Put in comments due to conflict with winappdriver libraries)
+    }
+
+    @AndroidFindBy(xpath="//*[@text='2. Inline']")
+    private AndroidElement btn_Inline;
+
+    /*
+      ################################################################################################
+      Method name: getBtn_Inline
+      Method description: This method provides access to private AndroidElement.
+      Method parameter: none
+      Method return: public AndroidElement
+      ################################################################################################
+    */
+    public AndroidElement getBtn_Inline() {
+        return btn_Inline;
+    }
+}
